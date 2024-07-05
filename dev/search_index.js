fd0ebbff952f3619991ad458ccf3afb0aa3bef90
Documenter.jl <documenter@juliadocs.github.io>
2024-07-05 08:17:21 +0000

    build based on f20e28a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MeanFieldGraph","category":"page"},{"location":"#MeanFieldGraph","page":"Home","title":"MeanFieldGraph","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MeanFieldGraph.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MeanFieldGraph]","category":"page"}]
+}
