f44f790d9f3da5a030171be9a091feaeaf20e039
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2024-11-21 13:46:20 +0100

    init

---
+++ examples/CLO24_plot_figure1.jl
@@ -0,0 +1,42 @@
+include("functions_estimation.jl")
+begin # Load 
+    df = CSV.read("data/estimators_vary_Δ.csv", DataFrame)
+    open("data/estimators_vary_Δ.toml") do io
+        toml2meta!(df, io)
+    end
+    df_inf = CSV.read("data/estimators_vary_Δ_inf.csv", DataFrame)
+    open("data/estimators_vary_Δ_inf.toml") do io
+        toml2meta!(df_inf, io)
+    end
+end
+
+begin # Compute absolute errors and medians
+    errors, errors_inf = estimators2errors.((df, df_inf))
+    q50, q50_inf = columnquantile.((errors, errors_inf), .5)
+end
+
+begin # Left plot 
+    selection = (q50.parameter .== 0)
+    plot(yaxis=:log, legend_columns=3)
+    for id in 1:6
+        plot!(q50[selection,1], q50[selection,id+2], color=id, label=names(q50)[id+2])
+        scatter!((q50[end,1], q50_inf[1,id+1]), color=id, marker = :o, label=false)
+    end
+    xlabel!("T")
+    ylims!(1e-4, 1)
+    ylabel!("absolute error")
+    title!("Estimation error for all parameters, Δ=NaN")
+end
+
+begin # Right plot 
+    selection = (q50.parameter .== 1)
+    plot(yaxis=:log, legend_columns=3)
+    for id in 1:6
+        plot!(q50[selection,1], q50[selection,id+2], color=id, label=names(q50)[id+2])
+        scatter!((q50[end,1], q50_inf[1,id+1]), color=id, marker = :o, label=false)
+    end
+    xlabel!("T")
+    ylims!(1e-4, 1)
+    ylabel!("absolute error")
+    title!("Estimation error for all parameters, Δ=1")
+end
---
+++ examples/CLO24_simulation_figure1.jl
@@ -0,0 +1,38 @@
+include("functions_estimation.jl")
+Random.seed!(1)
+
+## Informations for reproducibility
+
+## Default values
+default_values = (N = 500,
+r₊ = .5,
+β = .5,
+λ = .5,
+p = .5,
+Δ = 1,
+Nsimu = Int(1e2))
+
+# Specific values
+T = Int(1e5)
+length_tvec = 100
+tmin = 10
+tvec = floor.(Int,collect(range(tmin,T,length_tvec)))
+
+
+begin   # Simulation 
+    Δvec = [0,1]
+    df, df_inf = estimatorstable(:Δ, Δvec, default_values, tvec)
+
+    paramstring = metadata(df, "Varying parameter")
+    begin   # Save
+        CSV.write("data/estimators_vary_Δ.csv", df)
+        open("data/estimators_vary_Δ.toml", "w") do io
+            print(io, meta2toml(df))
+        end
+    
+        CSV.write("data/estimators_vary_Δ_inf.csv", df_inf)
+        open("data/estimators_vary_Δ_inf.toml", "w") do io
+            print(io, meta2toml(df_inf))
+        end
+    end;
+end;
