1f6b0e0d02c893c512b1f43e910e67042d5588d1
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2024-11-22 15:08:15 +0100

    remove Makie

--- examples/CO24_plot_figure2.jl
+++ examples/CO24_plot_figure2.jl
@@ -11,15 +11,12 @@ T = errors_and_std.T
 N = errors_and_std.parameter
 proba = errors_and_std.proba_exact_recovery
 
-using GLMakie
-begin
-    fig = Figure()
-    ax = Axis(fig[1, 1], xlabel = "T", ylabel = "N", title = "Probability of exact recovery")
-    hm = Makie.heatmap!(T, N, proba, interpolate = true)
-    lines!(ax, 0:100:T[end], sqrt.(0:100:T[end]), color = :red)
-    Makie.xlims!(ax, T[1], T[end])
-    Makie.ylims!(ax, N[1], N[end])
-    Colorbar(fig[:, end+1], hm)
-    fig
+begin # Plot
+    Plots.heatmap(unique(T), unique(N), transpose(reshape(proba, (20,21))))
+    plot!(0:100:T[end], sqrt.(0:100:T[end]), color = :green1, label=L"T = N^2")
+    xlims!(T[1], T[end])
+    ylims!(N[1], N[end])
+    xlabel!(L"T")
+    ylabel!(L"N")
 end
-# line which correspond to T = 4*N^2
+savefig("plots/CO24/colorplot.pdf")
