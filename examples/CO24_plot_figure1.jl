0eccbd937947385da7270ea3235e93e9c7c75891
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2024-11-22 15:08:28 +0100

    add savefig

--- examples/CO24_plot_figure1.jl
+++ examples/CO24_plot_figure1.jl
@@ -5,27 +5,32 @@ paramstring = "N"
 df = estimatorsload("data/CO24/classification_vary_"*paramstring)
 errors_and_std = proportion2errors(df)
 plotclassification(errors_and_std)
+savefig("plots/CO24/classification_vary_"*paramstring*".pdf")
 
 # r₊ varies
 paramstring = "r₊"
 df = estimatorsload("data/CO24/classification_vary_"*paramstring)
 errors_and_std = proportion2errors(df)
 plotclassification(errors_and_std)
+savefig("plots/CO24/classification_vary_"*paramstring*".pdf")
 
 # β varies
 paramstring = "β"
 df = estimatorsload("data/CO24/classification_vary_"*paramstring)
 errors_and_std = proportion2errors(df)
 plotclassification(errors_and_std)
+savefig("plots/CO24/classification_vary_"*paramstring*".pdf")
 
 # λ varies
 paramstring = "λ"
 df = estimatorsload("data/CO24/classification_vary_"*paramstring)
 errors_and_std = proportion2errors(df)
 plotclassification(errors_and_std)
+savefig("plots/CO24/classification_vary_"*paramstring*".pdf")
 
 # p varies
 paramstring = "p"
 df = estimatorsload("data/CO24/classification_vary_"*paramstring)
 errors_and_std = proportion2errors(df)
 plotclassification(errors_and_std)
+savefig("plots/CO24/classification_vary_"*paramstring*".pdf")
