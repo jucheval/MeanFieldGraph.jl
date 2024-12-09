7157f36aabdfbec4306b35c2e9e7dd788ba3853f
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2024-12-09 14:48:18 +0100

    typos

--- src/plots.jl
+++ src/plots.jl
@@ -3,7 +3,7 @@ import Plots.plot
 """
     plot(data::DiscreteTimeData)
 
-Plot a black and white image corresponding to `data`. Black pixels correpond to the value `1`. The x-axis correspond to time and y-axis correpond to components.
+Plot a black and white image corresponding to `data`. Black pixels correspond to the value `1`. The x-axis correspond to time and y-axis correspond to components.
 """
 function plot(data::DiscreteTimeData)
     N, T = size(data)
