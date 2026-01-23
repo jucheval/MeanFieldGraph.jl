c49ddced115f346880a6c230816c98c9d16b9110
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2026-01-23 15:02:42 +0100

    add unit tests

--- test/classification.jl
+++ test/classification.jl
@@ -1,15 +1,34 @@
 @testset "classification.jl" begin
+    data = DiscreteTimeData([
+        [1 0 0]
+        [0 1 1]
+        [1 0 0]
+        [1 1 0]
+    ])
+
     @test begin
-        data = DiscreteTimeData([
-            [1 0 0]
-            [0 1 1]
-            [1 0 0]
-            [1 1 0]
-        ])
         handcomputation = [
             1 - 2 * 1 / 3, 1 / 2 - 2 * 2 / 3, 1 - 2 * 1 / 3, 3 / 2 - 2 * 2 / 3
         ]
         MF.covariance_vector(data) == handcomputation
+    end
+
+    @test begin
+        handcomputation_Xs =
+            1 / 2 * [
+                [0 + 0 0 + 0 0 + 0 0 + 0]
+                [1 + 0 0 + 1 1 + 0 1 + 1]
+                [0 + 0 0 + 0 0 + 0 0 + 0]
+                [1 + 0 0 + 0 1 + 0 1 + 0]
+            ]
+        handcomputation_Zs =
+            1 / 9 * [
+                [1 * 1 1 * 2 1 * 1 1 * 2]
+                [2 * 1 2 * 2 2 * 1 2 * 2]
+                [1 * 1 1 * 2 1 * 1 1 * 2]
+                [2 * 1 2 * 2 2 * 1 2 * 2]
+            ]
+        MF.covariance_matrix(data) == handcomputation_Xs .- handcomputation_Zs
     end
 
     @test begin
@@ -19,12 +38,23 @@
     end
 
     @test begin
-        data = DiscreteTimeData([
-            [1 0 0]
-            [0 1 1]
-            [1 0 0]
-            [1 1 0]
-        ])
         classification(data) == [true, false, true, true]
+    end
+
+    @testset "covariance matrix and vector" begin
+        for _ in 1:10
+            Tsimu = Int(1e3)
+            N = 100
+            r₊ = rand()
+            excitatory = MF.N2excitatory(N, r₊)
+            β = rand()
+            λ = 0.3 + 0.7 * rand()
+            p = rand()
+            model = MarkovChainModel(β * λ, λ, p)
+            data = rand(model, N, r₊, Tsimu)
+            cov_vec = MeanFieldGraph.covariance_vector(data)
+            cov_mat = MeanFieldGraph.covariance_matrix(data)
+            @test cov_vec ≈ sum(cov_mat; dims=1)[1, :]
+        end
     end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,27 @@
-using MeanFieldGraph
-using Test
-using Distributions, LinearAlgebra
-using Random
+using Aqua
 using Clustering
+using Distributions
+using Documenter
+using JET
+using LinearAlgebra
+using MeanFieldGraph
 import MeanFieldGraph as MF
+using Random
+using Test
 
-@testset "MeanFieldGraph.jl" begin
+@testset verbose = true "MeanFieldGraph.jl" begin
+    @testset "Code quality (Aqua.jl)" begin
+        Aqua.test_all(MeanFieldGraph; ambiguities=false, deps_compat=(; check_extras=false))
+    end
+    @testset "Type stability (JET.jl)" begin
+        # test on 1.11 at a minimum and not on pre-release 
+        if VERSION >= v"1.12" && isempty(VERSION.prerelease)
+            JET.test_package(MeanFieldGraph; target_modules=(MeanFieldGraph,))
+        end
+    end
+    @testset "Doctests" begin
+        doctest(MeanFieldGraph)
+    end
     include("model.jl")
     include("simulate.jl")
     include("estimation.jl")
